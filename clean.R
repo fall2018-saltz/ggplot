7f599f78b98f50940830788c8c92490c70452242
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:41:48 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,4 +1,5 @@
 
-dfStates<- function(states) {states<- states[c(-1,-53),-1:-4] 
-colnames(states)<- c("stateName", "population", "popOver18", "percentOver18") 
+dfStates<- function(states) {
+  states<- states[c(-1,-53),-1:-4] 
+  colnames(states)<- c("stateName", "population", "popOver18", "percentOver18") 
 
