c6003ef621c2bb480e6a2a3fc9eebb2f9a073d4b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:43:05 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,6 +1,8 @@
 
-dfStates<- function(states) {
+df_fun <- function(states) {
   states<- states[c(-1,-53),-1:-4] 
   colnames(states)<- c("stateName", "population", "popOver18", "percentOver18") 
   return(states)
-
+  }
+ 
+dfStates<- df_fun(raw_data)
