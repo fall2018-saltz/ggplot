aeb925379742801ae536a843bb8b902a6af63777
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 13:33:08 -0400

    Saving script2537.R

--- script2537.R
+++ script2537.R
@@ -4,7 +4,7 @@ library(ggplot2)
 g2<- ggplot(result, aes(x= result$Murder)) + geom_histogram(binwidth = 5, color = "black", fill= "white")+ ggtitle("states murder rate")
 g2
 
-g1<- ggplot(result, aes(x= result$population)) + geom_histogram(binwidth = 5, color = "black", fill= "white")+ ggtitle("states population")
+g1<- ggplot(result, aes(x= result$population)) + geom_histogram(binwidth = 50000, color = "black", fill= "white")+ ggtitle("states population")
 g1
 
 
