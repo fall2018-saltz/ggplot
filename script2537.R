6241c87c4edd06bdbee605d78fe8e3c869b97d03
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 00:39:08 -0400

    Saving script2537.R

--- script2537.R
+++ script2537.R
@@ -14,9 +14,14 @@ g5<-ggplot(result, aes(x= result$n , y= result$Murder)) +
   geom_col( color="blue", fill="white") + theme(axis.text.x = element_text(angle = 45, hjust=1))
 g5
 
-g6<-ggplot(result, aes(x= reorder(result$stateName, result$Murder), y= result$Murder)) +
-  geom_col( color="blue", fill="white") + theme(axis.text.x = element_text(angle = 45, hjust=1))+ coord_flip()
-h + labs(x= "State", "Murder rate")
+g6<-ggplot(result, aes(x= result$n , y= result$Murder)) +
+  geom_col( color="blue", fill="white") + theme(axis.text.x = element_text(angle = 45, hjust=1))
+g6 + labs(x= "State", "Murder rate") + ggtitle("Total Murders")
+
+g7<-ggplot(result, aes(x= reorder(result$stateName, result$Murder), y= result$Murder)) +
+  geom_col( color="blue", fill="white") + theme(axis.text.x = element_text(angle = 45, hjust=1))
+g7 + labs(x= "State", "Murder rate") +ggtitle("Total Murders")
+
 
 
 
