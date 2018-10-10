bbb3bb9dc430f82f206cd4cf0811de1a468f08ad
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 01:21:48 -0400

    Saving script2537.R

--- script2537.R
+++ script2537.R
@@ -4,13 +4,13 @@ library(ggplot2)
 g2<- ggplot(result, aes(x= result$Murder)) + geom_histogram(binwidth = 5, color = "black", fill= "white")+ ggtitle("states murder rate")
 g2
 
-g3<- ggplot(result, aes(x= factor(0), y= result$population)) + geom_boxplot()+ ggtitle("states population boxplot")
-g3
 
-g2<- ggplot(result, aes(x= result$Murder)) + geom_histogram(binwidth = 5, color = "black", fill= "white")+ ggtitle("states murder rate")
-g2g2<- ggplot(result, aes(x= result$Murder)) + geom_histogram(binwidth = 5, color = "black", fill= "white")+ ggtitle("states murder rate")
+
+g2<- ggplot(result, aes(x= result$Assault)) + geom_histogram(binwidth = 5, color = "black", fill= "white")+ ggtitle("states murder rate")
 g2
-g2<- ggplot(result, aes(x= result$Murder)) + geom_histogram(binwidth = 5, color = "black", fill= "white")+ ggtitle("states murder rate")
+g2<- ggplot(result, aes(x= result$UrbanPop)) + geom_histogram(binwidth = 5, color = "black", fill= "white")+ ggtitle("states murder rate")
+g2
+g2<- ggplot(result, aes(x= result$Rape)) + geom_histogram(binwidth = 5, color = "black", fill= "white")+ ggtitle("states murder rate")
 g2
 
 
