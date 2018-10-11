98618479176399aa76e8c19f4daf12f16a5e0db1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:44:46 -0400

    Saving script3020.R

--- script3020.R
+++ script3020.R
@@ -1,8 +1,9 @@
 
 library(ggplot2) ##library() is used to unpack the gglpot2 package and be able to use the functions within it is 
-g5<-ggplot(result, aes(x= result$n , y= result$Murder)) +
-  geom_col( color="blue", fill="white") 
 
+## To plot a bar graph, geom_col()cis used. Here states names are taken on x axis and murder rate on y axis.
+g5<-ggplot(result, aes(x= result$n , y= result$Murder)) + geom_col( color="blue", fill="white") 
+g5
 g6<-ggplot(result, aes(x= result$n , y= result$Murder)) + geom_col( color="blue", fill="white") + theme(axis.text.x = element_text(angle = 45, hjust=1))
 g6 + labs(x= "State", "Murder rate") + ggtitle("Total Murders")
 g6
