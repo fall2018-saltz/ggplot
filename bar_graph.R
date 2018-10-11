286589a5e896812174906ad5fa7cfeea3932e1da
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:08:16 -0400

    Saving bar_graph.R

--- bar_graph.R
+++ bar_graph.R
@@ -7,17 +7,17 @@ result$tot_mur= (result$population/100000)* result$Murder
 result$states_mur= round(result$tot_mur)
 
 ## To plot a bar graph, geom_col()cis used. Here states names are taken on x axis and murder rate on y axis.
-g5<-ggplot(result, aes(x= result$n , y= result$Murder)) + geom_col( color="blue", fill="white") 
+g5<-ggplot(result, aes(x= result$n , y= result$states_mur)) + geom_col( color="blue", fill="white") 
 g5
 
 
 ## On the avove graph, theme() is included to change the x axis and make it readable. Also, titile is added using ggtitle() and labs() give the labels to the x and y axis.
-g6<-ggplot(result, aes(x= result$n , y= result$Murder)) + geom_col( color="blue", fill="white") + theme(axis.text.x = element_text(angle = 45, hjust=1))
+g6<-ggplot(result, aes(x= result$n , y= result$states_mur)) + geom_col( color="blue", fill="white") + theme(axis.text.x = element_text(angle = 45, hjust=1))
 g6 + labs(x= "State", "Murder rate") + ggtitle("Total Murders")
 g6
 
 ## On the above graph, x axis is sorted using reorder() in the aes() in the ggplot()
-g7<-ggplot(result, aes(x= reorder(result$n, result$Murder), y= result$Murder)) +
+g7<-ggplot(result, aes(x= reorder(result$n, result$Murder), y= result$states_mur)) +
   geom_col( color="blue", fill="white") + theme(axis.text.x = element_text(angle = 45, hjust=1))
 g7 + labs(x= "State", "Murder rate") +ggtitle("Total Murders")
 g7
