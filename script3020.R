1aa1a11ae2a64bf09d4490d741a1ddcdc5c0ad31
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:47:33 -0400

    Saving script3020.R

--- script3020.R
+++ script3020.R
@@ -4,6 +4,9 @@ library(ggplot2) ##library() is used to unpack the gglpot2 package and be able t
 ## To plot a bar graph, geom_col()cis used. Here states names are taken on x axis and murder rate on y axis.
 g5<-ggplot(result, aes(x= result$n , y= result$Murder)) + geom_col( color="blue", fill="white") 
 g5
+
+
+## On the avove graph, theme() is included to change the x axis and make it readable. Also, titile is added using ggtitle() and labs() give the labels to the x and y axis.
 g6<-ggplot(result, aes(x= result$n , y= result$Murder)) + geom_col( color="blue", fill="white") + theme(axis.text.x = element_text(angle = 45, hjust=1))
 g6 + labs(x= "State", "Murder rate") + ggtitle("Total Murders")
 g6
