a89f41862060bcafb5f9aae78e06736a651b647d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:00:34 -0400

    Saving script3020.R

--- script3020.R
+++ script3020.R
@@ -8,12 +8,16 @@ g6<-ggplot(result, aes(x= result$n , y= result$Murder)) +
 g6 + labs(x= "State", "Murder rate") + ggtitle("Total Murders")
 g6
 
-g6 <- ggplot(data= result, aes(x= result$n, y=result$MurderMurder, fill=result$percentOver18)) 
-g6 <- gnew + geom_col(stat="identity")
-g6 <- gnew + scale_fill_gradient(low = "plum2", high = "plum4", guide = "colorbar") 
-g6
 
 g7<-ggplot(result, aes(x= reorder(result$n, result$Murder), y= result$Murder)) +
   geom_col( color="blue", fill="white") + theme(axis.text.x = element_text(angle = 45, hjust=1))
 g7 + labs(x= "State", "Murder rate") +ggtitle("Total Murders")
 g7
+
+
+g8 <- ggplot(result, aes(x= reorder(result$n, y=result$MurderMurder, fill=result$percentOver18))) 
+ + geom_col(stat="identity") + theme(axis.text.x = element_text(angle = 45, hjust=1)) + labs(x= "State", "Murder rate") +ggtitle("Total Murders")
+g8 <- g8 + scale_fill_gradient(low = "plum2", high = "plum4", guide = "colorbar") 
+g8
+
+
