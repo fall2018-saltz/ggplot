42a9c45e91bdc1ad805f7b93089827a9c2291352
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:52:25 -0400

    Saving scatter.R

--- scatter.R
+++ scatter.R
@@ -1,5 +1,5 @@
 
-library(ggplot2)
+library(ggplot2) ## library() is used to unpack the package and be able to use the function in the ggplot2 package.
 g9<-ggplot(result, aes(x= result$population, y= result$percentOver18, size= result$Murder)) +
   geom_point( shape=19, color= "red") 
 
