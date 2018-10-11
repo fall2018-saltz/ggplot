176f845e5c1d57545ad4bb5821ba5c571b37aa7a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:08:39 -0400

    Saving script3020.R

--- script3020.R
+++ script3020.R
@@ -4,6 +4,8 @@ library(ggplot2) ##library() is used to unpack the gglpot2 package and be able t
 library(dplyr) ## library() will allow us to use the functions in dplyr package
 result_group<- group_by(result, n) groub_by function will group the result dataframe into the number of states
 states_mur_tot<- summarise(result_group, tot_mur= sum(Murder)) ## summarise() will perform sum() on the murder rate in each group.
+#states_mur_tot is a dataframe consisting of sates and their murder rate
+
 
 ## To plot a bar graph, geom_col()cis used. Here states names are taken on x axis and murder rate on y axis.
 g5<-ggplot(result, aes(x= result$n , y= result$Murder)) + geom_col( color="blue", fill="white") 
