ea4b8558c963ead65f3dcc4002a027a0eb77f33b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:17:59 -0400

    Saving script2537.R

--- script2537.R
+++ script2537.R
@@ -28,25 +28,5 @@ g4
 ##I like boxplot more than an histogram personaly, because just by looking at the graph I am able to tell the maximum, minimum median, 25th percentile and 75th pertentiles.
 
 
-g5<-ggplot(result, aes(x= result$n , y= result$Murder)) +
-  geom_col( color="blue", fill="white") + theme(axis.text.x = element_text(angle = 45, hjust=1))
-g5
-
-g6<-ggplot(result, aes(x= result$n , y= result$Murder)) +
-  geom_col( color="blue", fill="white") + theme(axis.text.x = element_text(angle = 45, hjust=1))
-g6 + labs(x= "State", "Murder rate") + ggtitle("Total Murders")
-g6
-
-g7<-ggplot(result, aes(x= reorder(result$n, result$Murder), y= result$Murder)) +
-  geom_col( color="blue", fill="white") + theme(axis.text.x = element_text(angle = 45, hjust=1))
-g7 + labs(x= "State", "Murder rate") +ggtitle("Total Murders")
-g7
-
-
-g8<-ggplot(result, aes(x= result$population, y= result$percentOver18, size= result$Murder)) +
-  geom_point( shape=19, color="blue", fill="red") + theme(axis.text.x = element_text(angle = 45, hjust=1))
-g8
-c1<- ggplot(result, aes(x= result$population)) + geom_histogram(binwidth= 779147, bins= 30, color = "black", fill= "white")+ ggtitle("states population")
-c1
 
 
