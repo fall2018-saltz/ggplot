4dd2dc9467ffe5f76a2339e641d278c203b038b7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:45:42 -0400

    Saving merged_data2.R

--- merged_data2.R
+++ merged_data2.R
@@ -2,5 +2,5 @@
 library(ggmap)
 dfs<- data.frame(state.name,state.center,state.area, stringsAsFactors = F)
  
-res<- merge(result,dfs, by.x= result$n, by.y= dfs$state.name, stringsAsFactors= FALSE)
+res<- merge(result,dfs, by.x= "n", by.y= "state.name", stringsAsFactors= FALSE)
 res
