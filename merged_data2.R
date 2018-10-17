f44978e0bfdcbebfece277d7a4588e4df37409c6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 16:45:26 -0400

    Saving merged_data2.R

--- merged_data2.R
+++ merged_data2.R
@@ -1,13 +1,14 @@
 
-library(ggplot2)
-library(ggmap)
-dfs<- data.frame(state.name,state.center,state.area, stringsAsFactors = F)
+library(ggplot2) ## library function is used to be able to use the function of the package ggplot2
+library(ggmap) ## library function is used to be able to use the function of the package ggmap
+
+dfs<- data.frame(state.name,state.center,state.area, stringsAsFactors = F) ## A new dataframe is created having state name, state centre and state longitude and latitude.
  
-res<- merge(result,dfs, by.x= "n", by.y= "state.name", stringsAsFactors= FALSE)
+res<- merge(result,dfs, by.x= "n", by.y= "state.name", stringsAsFactors= FALSE) ## The dfs dateset is merged to the census dataset so that we have state location and area for all the statesin the census dataset.
 res
 
-us <- map_data("state")
-res$n<- tolower(res$n)
+us <- map_data("state") ##turn data from the maps package in to a data frame suitable for plotting with ggplot2
+res$n<- tolower(res$n) ##state names are converted to lowercase
 mp_det <- ggplot(res, aes(map_id = n))
 mp_det <- mp_det + geom_map(map = us, aes(fill = state.area), color = "black", na.rm = TRUE)
 mp_det <- mp_det + expand_limits(x = us$long, y = us$lat)
