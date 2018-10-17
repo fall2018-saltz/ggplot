31bcc02483a03789e8ef33e0499572e30a9b55ae
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:31:27 -0400

    Saving merged_data2.R

--- merged_data2.R
+++ merged_data2.R
@@ -1,1 +1,6 @@
 
+dfs<- data.frame(state.name,state.center,state.area, stringsAsFactors = F)
+head(result1)
+dfs$state.name<- 
+res<- merge(result,dfs, by.x= result$n, by.y= dfs$state.name, stringsAsFactors= FALSE)
+
