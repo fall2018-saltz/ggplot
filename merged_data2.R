13b4434f7696584d7066391bc308c37c9aab31e6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:54:13 -0400

    Saving merged_data2.R

--- merged_data2.R
+++ merged_data2.R
@@ -13,3 +13,18 @@ mp_det <- mp_det + expand_limits(x = us$long, y = us$lat)
 mp_det <- mp_det + scale_fill_gradient(low = "white", high = "blue", guide = "colorbar") 
 mp_det <- mp_det + ggtitle("Area of states") + labs(x = "Longitude", y = "Latitude") 
 mp_det
+
+mp_det1 <- ggplot(res, aes(map_id = n))
+mp_det1 <- mp_det1 + geom_map(map = us, aes(fill = res$Murder), color = "black", na.rm = TRUE)
+mp_det1 <- mp_det1 + expand_limits(x = us$long, y = us$lat)
+mp_det1 <- mp_det1 + scale_fill_gradient(low = "white", high = "red", guide = "colorbar") 
+mp_det1 <- mp_det1 + ggtitle("Area of states") + labs(x = "Longitude", y = "Latitude") 
+mp_det1
+
+mp_det2<- mp_det+ geom_point( data=res, aes(x= x, y= y, size= res$population))
+mp_det2+ ggtitle("Population of states")
+mp_det2
+
+latlon.nyc<- geocode(source = "dsk", "NYC, ny")
+mp_det3<- mp_det1 + geom_point(aes(x= latlon.nyc$lon, y= latlon.nyc$lat), color= "darkred", size= 3)
+mp_det3<- mp_det1 + xlim(latlon.nyc$lon-10, latlon.nyc$lon+10) + ylim(latlon.nyc$lat-10,latlon.nyc$lat+10) 
