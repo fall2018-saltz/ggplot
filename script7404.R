5dd0c41e1e1b4bd3e445b5bf2e54a0208a1cce8d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:41:08 -0400

    Saving script7404.R

--- script7404.R
+++ script7404.R
@@ -1,4 +1,8 @@
 
+library(ggmap)
+dfs<- data.frame(state.name,state.center,state.area, stringsAsFactors = F)
+res<- merge(result,dfs, by.x= result$n, by.y= dfs$state.name, stringsAsFactors= FALSE)
+
 us <- map_data("state")
 res$n<- tolower(res$n)
 mp_det <- ggplot(res, aes(map_id = n))
